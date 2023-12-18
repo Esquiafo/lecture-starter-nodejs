b0317f7b423e632903fc416590d85d29f295c613
esquiafo <leocapone@hotmail.com>
2023-12-17 23:16:42 -0300

    + feat(routes): endpoints

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -8,6 +8,20 @@ import {
 
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+router.get('/', (req, res, next) => {
+  
+});
+router.get('/:id', (req, res, next) => {
+  
+});
+router.post('/', (req, res, next) => {
+  
+});
+router.put('/:id', (req, res, next) => {
+  
+});
+router.delete('/:id', (req, res, next) => {
+  
+});
 
 export { router };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,20 @@ import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
 
-// TODO: Implement route controllers for user
+router.get('/', (req, res, next) => {
+  
+});
+router.get('/:id', (req, res, next) => {
+  
+});
+router.post('/', (req, res, next) => {
+  
+});
+router.put('/:id', (req, res, next) => {
+  
+});
+router.delete('/:id', (req, res, next) => {
+  
+});
 
 export { router };
